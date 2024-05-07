2951b6f26f281ac22eb136a7b7004004e70f0997
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-05-07 19:35:18 +0700

    fix: landing page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts.template')
 @section('content')
+    <div class="w-full flex flex-row justify-center items-center gap-4">
+        <div class="w-full flex flex-col justify-start items-start bg-neutral-300 px-4 py-4 rounded-lg gap-2">
+            <div class="px-2 py-2 w-fit rounded-full bg-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#7f7f7f" viewBox="0 0 256 256"><path d="M125.18,156.94a64,64,0,1,0-82.36,0,100.23,100.23,0,0,0-39.49,32,12,12,0,0,0,19.35,14.2,76,76,0,0,1,122.64,0,12,12,0,0,0,19.36-14.2A100.33,100.33,0,0,0,125.18,156.94ZM44,108a40,40,0,1,1,40,40A40,40,0,0,1,44,108Zm206.1,97.67a12,12,0,0,1-16.78-2.57A76.31,76.31,0,0,0,172,172a12,12,0,0,1,0-24,40,40,0,1,0-10.3-78.67,12,12,0,1,1-6.16-23.19,64,64,0,0,1,57.64,110.8,100.23,100.23,0,0,1,39.49,32A12,12,0,0,1,250.1,205.67Z"></path></svg>
+            </div>
+            <span class="text-base font-medium text-neutral-500">Jumlah Penduduk</span>
+            <span class="text-2xl font-bold text-black">2.100.800</span>
+        </div>
+        <div class="w-full flex flex-col justify-start items-start bg-neutral-300 px-4 py-4 rounded-lg gap-2">
+            <div class="px-2 py-2 w-fit rounded-full bg-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#7f7f7f" viewBox="0 0 256 256"><path d="M234.38,210a123.36,123.36,0,0,0-60.78-53.23,76,76,0,1,0-91.2,0A123.36,123.36,0,0,0,21.62,210a12,12,0,1,0,20.77,12c18.12-31.32,50.12-50,85.61-50s67.49,18.69,85.61,50a12,12,0,0,0,20.77-12ZM76,96a52,52,0,1,1,52,52A52.06,52.06,0,0,1,76,96Z"></path></svg>
+            </div>
+            <span class="text-base font-medium text-neutral-500">Jumlah Keluarga</span>
+            <span class="text-2xl font-bold text-black">2.100.800</span>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,17 @@
 <div class="fixed z-10 flex flex-row justify-between bg-[#0EA5E9] w-full h-[60px] items-center px-4 sm:px-8">
     <div class="flex flex-wrap justify-start items-center gap-1">
-        <div class="flex flex-row justify-center items-center">
+        <div class="flex flex-row justify-center items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M231.88,175.08A56.26,56.26,0,0,1,176,224C96.6,224,32,159.4,32,80A56.26,56.26,0,0,1,80.92,24.12a16,16,0,0,1,16.62,9.52l21.12,47.15,0,.12A16,16,0,0,1,117.39,96c-.18.27-.37.52-.57.77L96,121.45c7.49,15.22,23.41,31,38.83,38.51l24.34-20.71a8.12,8.12,0,0,1,.75-.56,16,16,0,0,1,15.17-1.4l.13.06,47.11,21.11A16,16,0,0,1,231.88,175.08Z"></path></svg>
             <span class="font-semibold text-xs text-white sm:text-base">022-6623181</span>
         </div>
         <span class="font-semibold text-base text-white">|</span>
-        <div class="flex flex-row justify-center items-center">
+        <div class="flex flex-row justify-center items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M224,48H32a8,8,0,0,0-8,8V192a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A8,8,0,0,0,224,48ZM98.71,128,40,181.81V74.19Zm11.84,10.85,12,11.05a8,8,0,0,0,10.82,0l12-11.05,58,53.15H52.57ZM157.29,128,216,74.18V181.82Z"></path></svg>
             <span class="font-semibold text-xs text-white sm:text-base">pemdes@jatiasih-soehat.desa.id</span>
         </div>
     </div>
-    <div class="flex flex-row justify-center items-center">
+    <div class="flex flex-row justify-center items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm88,104a87.62,87.62,0,0,1-6.4,32.94l-44.7-27.49a15.92,15.92,0,0,0-6.24-2.23l-22.82-3.08a16.11,16.11,0,0,0-16,7.86h-8.72l-3.8-7.86a15.91,15.91,0,0,0-11-8.67l-8-1.73L96.14,104h16.71a16.06,16.06,0,0,0,7.73-2l12.25-6.76a16.62,16.62,0,0,0,3-2.14l26.91-24.34A15.93,15.93,0,0,0,166,49.1l-.36-.65A88.11,88.11,0,0,1,216,128ZM40,128a87.53,87.53,0,0,1,8.54-37.8l11.34,30.27a16,16,0,0,0,11.62,10l21.43,4.61L96.74,143a16.09,16.09,0,0,0,14.4,9h1.48l-7.23,16.23a16,16,0,0,0,2.86,17.37l.14.14L128,205.94l-1.94,10A88.11,88.11,0,0,1,40,128Z"></path></svg>
         <span class="font-normal text-xs text-white sm:text-base">Kota Malang</span>
     </div>
 </div>
