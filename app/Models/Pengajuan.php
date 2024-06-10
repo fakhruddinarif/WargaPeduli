eac1d0f94c45726d2b63f7b3491735352f0d46b6
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-06-10 19:15:07 +0700

    fix: pengajuan

--- app/Models/Pengajuan.php
+++ app/Models/Pengajuan.php
@@ -16,7 +16,7 @@ class Pengajuan extends Model
     protected $keyType = "string";
     public $timestamps = true;
 
-    protected $fillable = ['dokumen', 'nkk', 'nik', 'nama', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'ibu_kandung', 'status_warga', 'telepon', 'username', 'password', 'status', 'rt_id'];
+    protected $fillable = ['nkk', 'dokumen_kk', 'nik', 'dokumen_ktp', 'nama', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'ibu_kandung', 'status_warga', 'status_keluarga', 'telepon', 'rt_id', 'status'];
 
     public function rukunTetangga() : BelongsTo
     {
