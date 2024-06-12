a19d5bd993982a781e279801a97a2ba4596ee218
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-06-12 23:04:50 +0700

    fix: kewalik tr e anjayyy

--- resources/views/warga/index.blade.php
+++ resources/views/warga/index.blade.php
@@ -305,8 +305,8 @@
                     </thead>
                      <tbody>
                         @if(count($historyReport) > 0)
-                            <tr class="bg-white border-b md:table-row">
-                                @foreach($historyReport as $value)
+                            @foreach($historyReport as $value)
+                                <tr class="bg-white border-b md:table-row">
                                     <td class="px-6 py-4">{{ date('d/m/Y', strtotime($value->tanggal)) }}</td>
                                     <td class="px-6 py-4"> {{ $value->keterangan }}</td>
                                     <td class="px-6 py-4">{{ $value->status }}</td>
@@ -317,8 +317,8 @@
                                             </button>
                                         </div>
                                     </td>
-                                @endforeach
-                            </tr>
+                                </tr>
+                            @endforeach
                         @else
                             <tr>
                                 <td colspan="4" class="w-full bg-white text-center py-4 font-medium text-sm">Tidak ada data</td>
