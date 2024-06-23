bfd4ac08703561fe135ecf2be1ad30e332469246
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2024-06-23 23:34:10 +0700

    fix: service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //if (env('APP_ENV') === 'production') $this->app['request']->server->set('HTTPS', true);
+        if (env('APP_ENV') === 'production') $this->app['request']->server->set('HTTPS', true);
     }
 
     /**
